b38f15b5408ca79a5326e69c4f6041e626025a80
biegt <31814968+biegt@users.noreply.github.com>
2020-10-25 10:30:52 +0100

    Update reducer.py

--- mapreduce/reducer.py
+++ mapreduce/reducer.py
@@ -6,13 +6,11 @@ last_key = None
 running_total = 0
 
 for line in sys.stdin:
-    # split line into key and value
+    
     line = line.strip().split()
     this_key, value = line
     value = int(value)
 
-    # This only works because the reducer
-    # receives its data sorted by key:
     if this_key == last_key:
         running_total = running_total + 1
     else:
